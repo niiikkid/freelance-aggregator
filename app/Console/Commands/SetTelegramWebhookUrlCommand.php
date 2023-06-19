35339d33315f7ddfd7646bff55076728fcaeb00c
niiikkid <niiikkid@gmail.com>
2023-06-20 00:08:15 +1000

    such and such

---
+++ app/Console/Commands/SetTelegramWebhookUrlCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Telegram\Bot\Laravel\Facades\Telegram;
+
+class SetTelegramWebhookUrlCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'telegram:set-webhook-url';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Telegram::setWebhook([
+            'url' => 'https://0bd0-217-150-75-43.ngrok-free.app/api/telegram-bot/webhook'
+        ]);
+    }
+}
